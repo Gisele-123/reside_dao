d595ba82d3614605df877efe36de354a6bb3c826
Gisele-123 <giselemigisha53@gmail.com>
2024-12-11 08:57:22 -0500

    feat: added links to other pages

--- src/reside_frontend/src/Pages/Header.jsx
+++ src/reside_frontend/src/Pages/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6">
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
           Reside
         </Typography>
+        <Button color="inherit" component={Link} to="/residence">Residence</Button>
+        <Button color="inherit" component={Link} to="/apartment">Apartment</Button>
+        <Button color="inherit" component={Link} to="/council">Council</Button>
       </Toolbar>
     </AppBar>
   );
